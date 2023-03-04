eb8923a915972185a79a680fd8227e1c94c886e2
uyenph <uyenph112@gmail.com>
2023-03-04 17:42:56 +0700

    rename column

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -2,3 +2,11 @@ with dim_product__source as(
   select *
   from `vit-lam-data.wide_world_importers.warehouse__stock_items`
 )
+
+,dim_product__rename_column as(
+  select 
+  stock_item_id as product_key
+  ,stock_item_name as product_name
+  ,brand as brand_name
+  from dim_product__source
+)
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,3 +1,27 @@
+with fact_sales_order_lines__source as(
+  select *
+  from `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+,fact_sales_order_lines__rename_column as(
+  select 
+  order_line_id as sales_order_line_key
+  ,stock_item_id as product_key
+  ,quantity
+  ,unit_price
+  from fact_sales_order_lines__source
+)
+
+,fact_sales_order_lines__cast_type as(
+  select
+  cast(sales_order_line_key as int) as sales__order_lines
+  ,cast(product_key as int) as product_key
+  ,cast(quantity as int) as quantity
+  ,cast(unit_price as numeric) as unit_price
+  from fact_sales_order_lines__rename_column
+)
+
+
 SELECT 
 cast(order_line_id as int) as sales_order_line_key
 ,cast(stock_item_id as int) as product_key
