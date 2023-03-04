644862b205285af075696479c6e192efe107515e
uyenph <uyenph112@gmail.com>
2023-03-04 18:03:51 +0700

    cte rename column

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -2,3 +2,10 @@ with dim_customer__source as(
   select *
   from `vit-lam-data.wide_world_importers.sales__customers`
 )
+
+,dim_customer__rename_column as(
+  select 
+  customer_id as customer_key
+  customer_name	as customer_name
+  from dim_customer__source
+)
