c68c5b83c13a49fae1aeaaed1df5cd6cbb8e5c57
uyenph <uyenph112@gmail.com>
2023-03-04 17:47:22 +0700

    cte rename column

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,3 +2,13 @@ with fact_sales_order_lines__source as(
   select *
   from `vit-lam-data.wide_world_importers.sales__order_lines`
 )
+
+,fact_sales_order_lines__rename_column as(
+  select 
+  order_line_id as sales_order_line_key
+  ,stock_item_id as product_key
+  ,quantity
+  ,unit_price
+  from fact_sales_order_lines__source
+)
+
