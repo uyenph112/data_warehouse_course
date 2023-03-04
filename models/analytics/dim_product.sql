07fd755aaab188805265b4d1ebd601998d3569ca
uyenph <uyenph112@gmail.com>
2023-03-04 17:41:35 +0700

    data source

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,4 @@
-SELECT 
-cast(stock_item_id as int)	as product_key
-,cast(stock_item_name as string) as	product_name
-,cast(brand as string) as	brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+with dim_product__source as(
+  select *
+  from `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
