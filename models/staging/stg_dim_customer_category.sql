2cd20b3dc54264364b36af7712f6e0846509dcf2
uyenph <uyenph112@gmail.com>
2023-03-11 16:30:37 +0700

    create view stg_dim_customer_category

---
+++ models/staging/stg_dim_customer_category.sql
@@ -0,0 +1,23 @@
+WITH stg_dim_customer_category__source AS(
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__customer_categories`
+)
+
+,stg_dim_customer_category__rename_column AS(
+  SELECT
+    customer_category_id AS customer_category_key
+    ,customer_category_name AS customer_category_name
+  FROM stg_dim_customer_category__source
+)
+
+,stg_dim_customer_category__cast_type AS(
+  SELECT
+    CAST(customer_category_key AS INT) AS customer_category_key
+    ,CAST(customer_category_name AS STRING) AS customer_category_name
+  FROM stg_dim_customer_category__rename_column
+)
+
+SELECT
+  customer_category_key
+  ,customer_category_name
+FROM stg_dim_customer_category__cast_type
