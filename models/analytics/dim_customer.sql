774db72a7e4f3a6961dc1df35a2ed832eaa3a620
uyenph <uyenph112@gmail.com>
2023-03-11 16:56:25 +0700

    add customer_cate_name, buying_group_name

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -22,8 +22,14 @@ with dim_customer__source as(
 )
 
 select
-customer_key
-,customer_name
-,customer_category_key
-,buying_group_key
-from dim_customer__cast_type
+dim_customer.customer_key
+,dim_customer.customer_name
+,dim_customer.customer_category_key
+,dim_customer_cate.customer_category_name
+,dim_customer.buying_group_key
+,dim_buying_group.buying_group_name
+from dim_customer__cast_type AS dim_customer 
+left join {{ ref('stg_dim_customer_category') }} dim_customer_cate
+  on dim_customer.customer_category_key = dim_customer_cate.customer_category_key
+left join {{ ref('stg_dim_sales_buying_group') }} dim_buying_group 
+ on dim_customer.buying_group_key = dim_buying_group.buying_group_key
