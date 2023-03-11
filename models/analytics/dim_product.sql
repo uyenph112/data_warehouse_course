deb6174ee4ad92168443d38e72cb23f8c1cbb6c2
uyenph <uyenph112@gmail.com>
2023-03-11 17:46:36 +0700

    add is_chiller_stock column

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,6 +9,7 @@ with dim_product__source as(
   ,supplier_id as supplier_key
   ,stock_item_name as product_name
   ,brand as brand_name
+  ,is_chiller_stock 
   from dim_product__source
 )
 
@@ -18,6 +19,7 @@ with dim_product__source as(
   ,cast(supplier_key as int) as supplier_key
   ,cast(product_name as string) as product_name
   ,cast(brand_name as string) as brand_name
+  ,cast(is_chiller_stock as boolean) as is_chiller_stock
   from dim_product__rename_column
 )
 
@@ -27,6 +29,7 @@ dim_product.product_key
 ,dim_product.product_name
 ,dim_supplier.supplier_name
 ,dim_product.brand_name
+,dim_product.is_chiller_stock
 FROM dim_product__cast_type AS dim_product
 LEFT JOIN {{ ref('dim_supplier')}} AS dim_supplier
   ON dim_product.supplier_key = dim_supplier.supplier_key
