c2fe8d836abbdc2b87573abc586bf10bc4c4e07f
uyenph <uyenph112@gmail.com>
2023-03-11 16:01:08 +0700

    create dim_supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,24 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+WITH dim_supplier__source AS(
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+,dim_supplier__rename_column AS(
+  SELECT
+    supplier_id AS supplier_key
+    ,supplier_name AS supplier_name
+  FROM dim_supplier__source
+)
+
+,dim_supplier__cast_type AS(
+  SELECT
+    CAST(supplier_key AS int) AS supplier_key
+    ,CAST(supplier_name AS string) AS supplier_name
+  FROM dim_supplier__rename_column
+)
+
+SELECT
+  supplier_key
+  ,supplier_name
+FROM dim_supplier__cast_type
