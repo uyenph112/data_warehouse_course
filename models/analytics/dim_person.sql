1bfdab90325da3b1b60925ea7f9ad51504fb172d
uyenph <uyenph112@gmail.com>
2023-03-16 19:14:32 +0700

    update dim_person table

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,24 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.application__people`
+WITH dim_person__source AS(
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+,dim_person__rename_column AS(
+  SELECT
+    person_id AS person_key
+    ,full_name AS full_name
+  FROM dim_person__source
+)
+
+,dim_person__cast_type AS(
+  SELECT
+    CAST(person_key AS int) AS person_key
+    ,CAST(full_name AS string) AS full_name
+  FROM dim_person__rename_column
+)
+
+SELECT
+  person_key
+  ,full_name
+FROM dim_person__cast_type
