4251be8250f581f75047391787eee4262a8bcbbd
uyenph <uyenph112@gmail.com>
2023-03-30 19:40:05 +0700

    modify dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -4,38 +4,86 @@ WITH dim_person__source AS(
   FROM `vit-lam-data.wide_world_importers.application__people`
 )
 
-,dim_person__rename_column AS(
+, dim_person__rename_column AS(
   SELECT
     person_id AS person_key
-    ,full_name AS full_name
+    , full_name AS person_name
+    , preferred_name AS preferred_person_name
+    , is_system_user AS is_system_user_boolean
+    , is_employee AS is_employee_boolean
+    , is_salesperson AS is_salesperson_boolean
   FROM dim_person__source
 )
 
-,dim_person__cast_type AS(
+, dim_person__cast_type AS(
   SELECT
-    CAST(person_key AS int) AS person_key
-    ,CAST(full_name AS string) AS full_name
+    CAST(person_key AS INT) AS person_key
+    , CAST(person_name AS STRING) AS person_name
+    , CAST(preferred_person_name AS STRING) AS preferred_person_name
+    , CAST(is_system_user_boolean AS BOOLEAN) AS is_system_user_boolean
+    , CAST(is_employee_boolean AS BOOLEAN) AS is_employee_boolean
+    , CAST(is_salesperson_boolean AS BOOLEAN) AS is_salesperson_boolean
   FROM dim_person__rename_column
 )
 
-,dim_person__add_undefined_record AS(
+, dim_person__convert_boolean AS(
+  SELECT 
+    *
+    , CASE 
+        WHEN is_system_user_boolean IS TRUE THEN 'System User'
+        WHEN is_system_user_boolean IS FALSE THEN 'Not System User'
+        WHEN is_system_user_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid'
+      END AS is_system_user
+    , CASE 
+        WHEN is_employee_boolean IS TRUE THEN 'Employee'
+        WHEN is_employee_boolean IS FALSE THEN 'Not Employee'
+        WHEN is_employee_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid'
+      END AS is_employee
+    , CASE 
+        WHEN is_salesperson_boolean IS TRUE THEN 'Salesperson'
+        WHEN is_salesperson_boolean IS FALSE THEN 'Not Salesperson'
+        WHEN is_salesperson_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid'
+      END AS is_salesperson
+  FROM dim_person__cast_type
+)
+
+, dim_person__add_undefined_record AS(
   SELECT
     person_key
-    ,full_name
-  FROM dim_person__cast_type
+    , person_name
+    , preferred_person_name
+    , is_system_user
+    , is_employee
+    , is_salesperson
+  FROM dim_person__convert_boolean
 
 UNION ALL 
   SELECT 
     0 AS person_key
-    , 'Undefined' as full_name
+    , 'Undefined' AS person_name
+    , 'Undefined' AS preferred_person_name
+    , 'Undefined' AS is_system_user
+    , 'Undefined' AS is_employee
+    , 'Undefined' AS is_salesperson
 
 UNION ALL 
   SELECT 
     -1 AS person_key
-    , 'Invalid' as full_name
+    , 'Invalid' AS person_name
+    , 'Invalid' AS preferred_person_name
+    , 'Invalid' AS is_system_user
+    , 'Invalid' AS is_employee
+    , 'Invalid' AS is_salesperson
 )
 
 SELECT
   person_key
-  ,full_name
+  , person_name
+  , preferred_person_name
+  , is_system_user
+  , is_employee
+  , is_salesperson
 FROM dim_person__add_undefined_record
--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -7,18 +7,33 @@ WITH dim_supplier__source AS(
 ,dim_supplier__rename_column AS(
   SELECT
     supplier_id AS supplier_key
-    ,supplier_name AS supplier_name
+    , supplier_name AS supplier_name
+    , payment_days
+    , primary_contact_person_id AS primary_contact_person_key
+    , alternate_contact_person_id AS alternate_contact_person_key
+    , supplier_category_id AS supplier_category_key
+    , delivery_method_id AS delivery_method_key
+    , delivery_city_id AS delivery_city_key
+    , postal_city_id AS postal_city_key
   FROM dim_supplier__source
 )
 
 ,dim_supplier__cast_type AS(
   SELECT
     CAST(supplier_key AS int) AS supplier_key
-    ,CAST(supplier_name AS string) AS supplier_name
+    , CAST(supplier_name AS string) AS supplier_name
+    , CAST(payment_days AS INT) AS payment_days
+    , CAST(primary_contact_person_key AS INT) AS primary_contact_person_key
+    , CAST(alternate_contact_person_key AS INT) AS alternate_contact_person_key
+    , CAST(supplier_category_key AS INT) AS supplier_category_key
+    , CAST(delivery_method_key AS INT) AS delivery_method_key
+    , CAST(delivery_city_key AS INT) AS delivery_city_key
+    , CAST(postal_city_key AS INT) AS postal_city_key
   FROM dim_supplier__rename_column
 )
 
 SELECT
   supplier_key
-  ,supplier_name
-FROM dim_supplier__cast_type
+  , supplier_name
+FROM dim_supplier__cast_type AS dim_supplier 
+LEFT JOIN 
