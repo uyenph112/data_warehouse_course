3e0b1f32838fb7bb673fb4da66a80351782975c4
uyenph <uyenph112@gmail.com>
2023-03-30 19:09:36 +0700

    modify dim_customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -73,13 +73,13 @@ SELECT
   , dim_customer.delivery_method_key
   , COALESCE(dim_delivery_method.delivery_method_name, 'Invalid') AS delivery_method_name
   , dim_customer.delivery_city_key
-  , COALESCE(dim_city_1.city_name, 'Invalid') AS delivery_city_name 
-  , COALESCE(dim_city_1.state_province_name, 'Invalid') AS delivery_state_province_name
-  , COALESCE(dim_city_1.country_name, 'Invalid') AS delivery_country_name
+  , COALESCE(dim_delivery_city.city_name, 'Invalid') AS delivery_city_name 
+  , COALESCE(dim_delivery_city.state_province_name, 'Invalid') AS delivery_state_province_name
+  , COALESCE(dim_delivery_city.country_name, 'Invalid') AS delivery_country_name
   , dim_customer.postal_city_key
-  , COALESCE(dim_city_2.city_name, 'Invalid') AS postal_city_name 
-  , COALESCE(dim_city_2.state_province_name, 'Invalid') AS postal_state_province_name
-  , COALESCE(dim_city_2.country_name, 'Invalid') AS postal_country_name
+  , COALESCE(dim_postal_city.city_name, 'Invalid') AS postal_city_name 
+  , COALESCE(dim_postal_city.state_province_name, 'Invalid') AS postal_state_province_name
+  , COALESCE(dim_postal_city.country_name, 'Invalid') AS postal_country_name
 FROM dim_customer__convert_boolean AS dim_customer 
 LEFT JOIN {{ ref('stg_dim_customer_category') }} AS dim_customer_cate
   ON dim_customer.customer_category_key = dim_customer_cate.customer_category_key
@@ -87,7 +87,7 @@ LEFT JOIN {{ ref('stg_dim_sales_buying_group') }} AS dim_buying_group
  ON dim_customer.buying_group_key = dim_buying_group.buying_group_key
 LEFT JOIN {{ ref('stg_dim_delivery_method') }} AS dim_delivery_method
   ON dim_customer.delivery_method_key = dim_delivery_method.delivery_method_key
-LEFT JOIN {{ ref('stg_dim_city') }} AS dim_city_1 
-  ON dim_customer.delivery_city_key = dim_city_1.city_key
-LEFT JOIN {{ ref('stg_dim_city') }} AS dim_city_2
-  ON dim_customer.postal_city_key = dim_city_2.city_key
+LEFT JOIN {{ ref('stg_dim_city') }} AS dim_delivery_city 
+  ON dim_customer.delivery_city_key = dim_delivery_city.city_key
+LEFT JOIN {{ ref('stg_dim_city') }} AS dim_postal_city
+  ON dim_customer.postal_city_key = dim_postal_city.city_key
