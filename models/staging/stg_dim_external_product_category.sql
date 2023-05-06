948ec895285b9d1a7f6e0df8d7337450d540c3bd
uyenph <uyenph112@gmail.com>
2023-05-06 18:03:34 +0700

    add level

--- models/staging/stg_dim_external_product_category.sql
+++ models/staging/stg_dim_external_product_category.sql
@@ -49,10 +49,65 @@ WITH stg_dim_external_product_category__source AS(
     ON product_category.parent_category_key = parent_category.product_category_key
 )
 
+, stg_dim_external_product_category__add_level AS(
+  SELECT
+    *
+    , product_category_name AS level_1
+    , 'Undefined' AS level_2
+    , 'Undefined' AS level_3
+    , 'Undefined' AS level_4
+  FROM stg_dim_external_product_category__parent_category_name
+  WHERE category_level = 1
+
+  UNION ALL
+ 
+  SELECT
+    *
+    , parent_category_name AS level_1
+    , product_category_name AS level_2
+    , 'Undefined' AS level_3
+    , 'Undefined' AS level_4
+  FROM stg_dim_external_product_category__parent_category_name
+  WHERE category_level = 2
+
+  UNION ALL
+
+  SELECT 
+    lv3.*
+    , lv2.parent_category_name AS level_1
+    , lv2.product_category_name AS level_2
+    , lv3.product_category_name AS level_3
+    , 'Undefined' AS level_4
+  FROM stg_dim_external_product_category__parent_category_name AS lv3
+  LEFT JOIN stg_dim_external_product_category__parent_category_name AS lv2 
+    ON lv3.parent_category_key = lv2.product_category_key
+  WHERE lv3.category_level = 3
+
+  UNION ALL
+
+  SELECT
+    lv4.*
+    , lv2.parent_category_name AS level_1
+    , lv2.product_category_name AS level_2
+    , lv3.product_category_name AS level_3 
+    , lv4.product_category_name AS level_4
+  FROM stg_dim_external_product_category__parent_category_name AS lv4
+  LEFT JOIN stg_dim_external_product_category__parent_category_name AS lv3 
+    ON lv4.parent_category_key = lv3.product_category_key
+  LEFT JOIN stg_dim_external_product_category__parent_category_name AS lv2 
+    ON lv3.parent_category_key = lv2.product_category_key
+  WHERE lv4.category_level = 4
+
+)
+
 SELECT
   product_category_key
   , product_category_name
   , parent_category_key
   , parent_category_name
   , category_level
-FROM stg_dim_external_product_category__parent_category_name
+  , level_1
+  , level_2
+  , level_3
+  , level_4
+FROM stg_dim_external_product_category__add_level
